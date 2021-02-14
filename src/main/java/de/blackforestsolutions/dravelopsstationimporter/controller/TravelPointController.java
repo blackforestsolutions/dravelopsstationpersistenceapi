f95e49345a967838952add5c67c758997acc4fc9
hennihaus <jan-hendrik.hausner@outlook.com>
2021-02-14 10:26:26 +0100

    adjusted travelPointsImporter interface #major

--- src/main/java/de/blackforestsolutions/dravelopsstationimporter/controller/TravelPointController.java
+++ src/main/java/de/blackforestsolutions/dravelopsstationimporter/controller/TravelPointController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("travelpoints")
+@RequestMapping("travelpoints/get")
 public class TravelPointController {
 
     private final TravelPointApiService travelPointApiService;
