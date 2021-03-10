7709922d045c79798f72070ffcd1325be11884a6
hennihaus <jan-hendrik.hausner@outlook.com>
2021-03-10 08:22:43 +0100

    updated datamodel made all IT running #patch

--- src/integrationtest/java/de/blackforestsolutions/dravelopsstationpersistenceapi/GtfsCallServiceIT.java
+++ src/integrationtest/java/de/blackforestsolutions/dravelopsstationpersistenceapi/GtfsCallServiceIT.java
@@ -1,33 +1,32 @@
 package de.blackforestsolutions.dravelopsstationpersistenceapi;
 
 import de.blackforestsolutions.dravelopsdatamodel.ApiToken;
-import de.blackforestsolutions.dravelopsstationpersistenceapi.configuration.GtfsTestConfiguration;
+import de.blackforestsolutions.dravelopsstationpersistenceapi.configuration.GtfsApiTokenConfiguration;
 import de.blackforestsolutions.dravelopsstationpersistenceapi.service.communicationservice.restcalls.CallService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 import java.io.File;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import static de.blackforestsolutions.dravelopsdatamodel.testutil.TestUtils.*;
+import static de.blackforestsolutions.dravelopsstationpersistenceapi.testutil.TestUtils.convertConfigTokenToApiTokens;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(GtfsTestConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GtfsCallServiceIT {
 
     @Autowired
-    private ApiToken.ApiTokenBuilder gtfsApiTokenIT;
+    private GtfsApiTokenConfiguration apiTokenConfiguration;
 
     @Autowired
     private CallService classUnderTest;
 
     @Test
     void test_download_gtfs_file() {
-        ApiToken testData = gtfsApiTokenIT.build();
+        ApiToken testData = convertConfigTokenToApiTokens(apiTokenConfiguration).get(0);
 
         File result = classUnderTest.getFile(testData.getGtfsUrl(), convertToHeaders(testData.getHeaders()));
 
